8dbf86e83006f0ee07ace66cd2a27cd64cfa49f4
frju365 <win10@tutanota.com>
2018-05-04 11:44:15 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,6 +6,28 @@
 nodejs_version=9
 
 #=================================================
+# BACKUP
+#=================================================
+
+HUMAN_SIZE () {	# Transforme une taille en Ko en une taille lisible pour un humain
+	human=$(numfmt --to=iec --from-unit=1K $1)
+	echo $human
+}
+
+CHECK_SIZE () {	# Vérifie avant chaque backup que l'espace est suffisant
+	file_to_analyse=$1
+	backup_size=$(du --summarize "$file_to_analyse" | cut -f1)
+	free_space=$(df --output=avail "/home/yunohost.backup" | sed 1d)
+
+	if [ $free_space -le $backup_size ]
+	then
+		ynh_print_err "Espace insuffisant pour sauvegarder $file_to_analyse."
+		ynh_print_err "Espace disponible: $(HUMAN_SIZE $free_space)"
+		ynh_die "Espace nécessaire: $(HUMAN_SIZE $backup_size)"
+	fi
+}
+
+#=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
 
