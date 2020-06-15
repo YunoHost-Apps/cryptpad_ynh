c1827db8934f0dd61b515e285a47cb41688ad0ae
ericgaspar <junk.eg@free.fr>
2020-06-15 17:46:34 +0200

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,60 +3,9 @@
 #=================================================
 # COMMON VARIABLES
 #=================================================
-nodejs_version=6
+nodejs_version=12
 
-#=================================================
 
-# Start or restart a service and follow its booting
-#
-# usage: ynh_check_starting "Line to match" [Log file] [Timeout] [Service name]
-#
-# | arg: Line to match - The line to find in the log to attest the service have finished to boot.
-# | arg: Log file - The log file to watch
-# | arg: Service name
-# /var/log/$app/$app.log will be used if no other log is defined.
-# | arg: Timeout - The maximum time to wait before ending the watching. Defaut 300 seconds.
-ynh_check_starting () {
-	local line_to_match="$1"
-	local service_name="${4:-$app}"
-	local app_log="${2:-/var/log/$service_name/$service_name.log}"
-	local timeout=${3:-300}
-
-	ynh_clean_check_starting () {
-		# Stop the execution of tail.
-		kill -s 15 $pid_tail 2>&1
-		ynh_secure_remove "$templog" 2>&1
-	}
-
-	echo "Starting of $service_name" >&2
-	systemctl stop $service_name
-	local templog="$(mktemp)"
-	# Following the starting of the app in its log
-	tail -F -n0 "$app_log" > "$templog" &
-	# Get the PID of the tail command
-	local pid_tail=$!
-	systemctl start $service_name
-
-	local i=0
-	for i in `seq 1 $timeout`
-	do
-		# Read the log until the sentence is found, that means the app finished to start. Or run until the timeout
-		if grep --quiet "$line_to_match" "$templog"
-		then
-			echo "The service $service_name has correctly started." >&2
-			break
-		fi
-		echo -n "." >&2
-		sleep 1
-	done
-	if [ $i -eq $timeout ]
-	then
-		echo "The service $service_name didn't fully started before the timeout." >&2
-	fi
-
-	echo ""
-	ynh_clean_check_starting
-}
 
 #=================================================
 
