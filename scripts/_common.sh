a4ba228ab0f9a09801580da7b3411bdc5036829d
ericgaspar <junk.eg@free.fr>
2021-01-22 14:23:35 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,3 +17,17 @@ nodejs_version="12"
 #=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
+
+
+# Execute a command as another user
+# usage: ynh_exec_as USER COMMAND [ARG ...]
+ynh_exec_as() {
+  local USER=$1
+  shift 1
+
+  if [[ $USER = $(whoami) ]]; then
+    eval "$@"
+  else
+    sudo -u "$USER" "$@"
+  fi
+}
