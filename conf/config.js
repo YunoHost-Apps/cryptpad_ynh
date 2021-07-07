2838fa9fc14dd26c529fedd8b37022aa131c450b
ericgaspar <junk.eg@free.fr>
2021-07-08 00:04:31 +0200

    Update config.js

--- conf/config.js
+++ conf/config.js
@@ -54,7 +54,7 @@ module.exports = {
  *  and it may have unintended consequences in practice.
  *
  */
-    httpUnsafeOrigin: 'https://127.0.0.1:__PORT__',
+    httpUnsafeOrigin: 'https://__DOMAIN__',
 
 /*  httpSafeOrigin is the URL that is used for the 'sandbox' described above.
  *  If you're testing or developing with CryptPad on your local machine then
