5ec12864f0a8de86aeb3f88a4759954b4cd1240e
tituspijean <tituspijean@outlook.com>
2021-11-27 12:10:20 +0100

    Revert "4.12.1"

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -63,7 +63,7 @@ echo "Handling asset at $asset_url"
 # Here we base the source file name upon a unique keyword in the assets url (admin vs. update)
 # Leave $src empty to ignore the asset
 case $asset_url in
-  *".tar.gz")
+  *".tar.gz"*)
     src="app"
     ;;
 esac
@@ -116,6 +116,9 @@ done
 #=================================================
 # GENERIC FINALIZATION
 #=================================================
+
+# Install moreutils, needed for sponge
+sudo apt-get install moreutils
 
 # Replace new version in manifest
 echo "$(jq -s --indent 4 ".[] | .version = \"$version~ynh1\"" manifest.json)" > manifest.json
